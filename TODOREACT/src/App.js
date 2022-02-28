bdad8897d054370f687e360aa71b7bc0e84cebfc
TheodoreNestel <73968042+TheodoreNestel@users.noreply.github.com>
2022-02-28 15:32:24 -0500

    ddsc

--- TODOREACT/src/App.js
+++ TODOREACT/src/App.js
@@ -16,11 +16,16 @@ function App() {
 
       <ul>
         {todoArray.map((todo , index) => <ListItem todo={todo.newTodo} key={index} checked={todo.checked} changeHandler={()=>{
-          const clone = [...todoArray]
-          clone[index].checked = !clone[index].checked
+          const clone = [...todoArray] //each todo item has the whole array of all todos with this change handler
+          clone[index].checked = !clone[index].checked // here we switch our bool to the opposite of what it is when clicked inside the todo compo
 
-          setTodoArray([...clone])
-        }}/>  )}
+          //////
+          //potentially add a check here to see if tthe value was changed and implement change if it is detected?**
+          //use this change handler to take care of this and add the input logic inside the ListItem component?
+          //////
+
+          setTodoArray([...clone]) //then we set the new array as the new main array 
+        }} />  )}
       </ul>
 
       <Input newTodo={newTodo} setNewTodo={setNewTodo} onSubmit={()=>{
@@ -33,3 +38,14 @@ function App() {
 }
 
 export default App;
+
+
+
+
+//functionality I need 
+
+//be able to click on a todo to edit the item inside 
+//order the todo's based on wether or not they were completed 
+
+
+
