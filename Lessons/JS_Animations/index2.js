f09a1d0e76e6d9f8eac187e4f8724878491dea80
TheodoreNestel <73968042+TheodoreNestel@users.noreply.github.com>
2022-06-02 11:39:12 -0400

    dsas
    ewqdw

---
+++ Lessons/JS_Animations/index2.js
@@ -0,0 +1,12 @@
+import anime from "animejs";
+
+alert("fuck")
+console.log("adskjfhasdf")
+const h1 = document.querySelector("h1")
+
+
+anime({
+    targets : ".test",
+    left: '240px',
+    easing: 'easeInOutQuad'
+})
